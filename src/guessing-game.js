030872c9170faa5f563e6d34f620f4839dbdc1b4
Anastasiia Sierbaieva <serbayeva@gmail.com>
2019-10-09 00:03:28 -0700

    add: task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+	constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this.midNumber = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	return this.midNumber = Math.round((this.min+this.max)/2);
     }
 
     lower() {
-
+    	this.max = this.midNumber;
     }
 
     greater() {
-
+    	this.min = this.midNumber;
     }
 }
 
